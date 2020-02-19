5cb4c562a8333e7b1a6a62686cc5dd7c21508505
Karen Daniela <kdaniimora@gmail.com>
2020-02-19 17:57:09 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,19 @@
 class Owner
   # code goes here
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    @species = species
+end 
+
+def name 
+  @name 
+end 
+
+def species
+  @species
+end 
+
+
 end
